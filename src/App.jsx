a88858688fa50fc37e88578ced5b4bad0dd94b54
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-26 12:51:51 +0200

    working PercentageWheel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react';
 import MostPlayedGames from './components/containers/MostPlayedGames';
+import PercentageWheel from './components/features/games/percentageWheel/percentageWheel';
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
       <p>Find your next gaming adventure from Steam's most popular titles</p>
       
       <MostPlayedGames />
+      <PercentageWheel 
+      percentage={50}
+      title="Teeest"
+      />
     </main>
   );
 }
---
+++ src/components/features/games/percentageWheel/PercentageWheel.jsx
@@ -0,0 +1,55 @@
+// src/components/features/games/percentageWheel/percentageWheel.jsx
+import React from 'react';
+import styles from './percentageWheel.module.css';
+
+const PercentageWheel = ({ percentage = 0, title, className, loading, error }) => {
+  
+  const percentageWheel = className ? `${styles.PercentageWheel} ${className}` : styles.PercentageWheel;
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  const circleWidth = 150;
+  const circleHeight = 150;
+  const radius = 50;
+  const dashArray = radius * Math.PI * 2;
+  const dashOffset = dashArray - (dashArray * percentage) / 100;
+  
+  return (
+    <div className={styles.PercentageWheel}>
+      <p>{title}</p>
+      <div className={styles.circleContainer}>
+        <svg
+          width={`${circleWidth}px`}
+          height={`${circleHeight}px`}
+          viewBox={`0 0 ${circleWidth} ${circleHeight}`}
+        >
+          <circle
+            cx={circleWidth / 2}
+            cy={circleHeight / 2}
+            strokeWidth="20px"
+            r={radius}
+            className={styles.circleBackground}
+          />
+          <circle
+            cx={circleWidth / 2}
+            cy={circleHeight / 2}
+            strokeWidth="20px"
+            r={radius}
+            style={{
+              strokeDasharray: dashArray,
+              strokeDashoffset: dashOffset,
+            }}
+            transform={`rotate(-90 ${circleWidth / 2} ${circleHeight / 2})`}
+            className={styles.circlePercentageBlue}
+          />
+        </svg>
+        <p className={styles.percentageText}>
+          {percentage}%
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PercentageWheel;
